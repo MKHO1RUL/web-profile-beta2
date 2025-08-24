ccf34c6cc01977f1ffa798549085e25708cb97b3
Muhammad Khoirul Irsyadul Ibad <irulkhoirul414@gmail.com>
2025-08-24 20:03:20 +0700

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -45,7 +45,6 @@ export default function AboutSection() {
         <h2 className="text-4xl md:text-5xl font-bold mb-4 bg-gradient-to-r from-orange-400 to-blue-400 bg-clip-text text-transparent">
           My Profile
         </h2>
-        <p className="text-orange-200 text-lg">A little about me..</p>
       </motion.div>
 
       <div className="max-w-6xl mx-auto">
