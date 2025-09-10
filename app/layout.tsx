9714270e8caadb4c9f828f7f3fa799ac86aea3f5
MKHO1RUL <irulkhoirul414@gmail.com>
2025-09-10 22:04:18 +0700

    v0.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'MKII',
-  description: 'Muhammad Khoirul Portofolio Website',
+  title: 'MKII Profile',
+  description: 'Muhammad Khoirul AI Engineer Portofolio Website',
   generator: 'v0.dev',
 }
 
